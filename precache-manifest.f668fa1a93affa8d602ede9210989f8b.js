ffa022cf144dff26a32a545db943461504f0d24c
jung485 <jgh3557@gmail.com>
2020-02-21 16:45:41 +0900

    Updates

---
+++ precache-manifest.f668fa1a93affa8d602ede9210989f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46767b8154e7a8f3a921b0518a79985",
+    "url": "/app2020/index.html"
+  },
+  {
+    "revision": "dddecdb395003f581d26",
+    "url": "/app2020/static/css/main.ac86a772.chunk.css"
+  },
+  {
+    "revision": "80a6c1649941c827aeb6",
+    "url": "/app2020/static/js/2.302d6766.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/app2020/static/js/2.302d6766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddecdb395003f581d26",
+    "url": "/app2020/static/js/main.cfb991a8.chunk.js"
+  },
+  {
+    "revision": "1679f4c08cd3c495a40d",
+    "url": "/app2020/static/js/runtime-main.7aec28e5.js"
+  }
+]);
